943265e62d1870495467a99f55a83ecf3e5c82e8
tanisshka <tanishkapatil0102@gmail.com>
2026-02-22 23:24:10 +0530

    Initial commit

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def predict_datapoint():
         
         predict_pipeline=PredictPipeline()
         results=predict_pipeline.predict(pred_df)
-        return render_template("home.html",results=results[0])
+        return render_template("result.html", results=results[0])
     
 if __name__=="__main__":
     app.run(host="0.0.0.0",debug=True)
