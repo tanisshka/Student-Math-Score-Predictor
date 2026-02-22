f0e31dc349ad433757b5ed86365cdb4fe42f596f
tanisshka <tanishkapatil0102@gmail.com>
2026-02-22 17:35:09 +0530

    Hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,7 +48,39 @@ class ModelTrainer:
                 "AdaBoost Regressor":AdaBoostRegressor()
             }
 
-            model_report:dict=evaluate_models(X_Train,y_train,X_Test,y_test,models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest":{
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neigbours Regressor":{
+                    'n_neighbors':[3,5,7,9,11]
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+            
+
+            model_report:dict=evaluate_models(X_Train,y_train,X_Test,y_test,models,params)
 
             #best model score from dictionary
             best_model_score=max(sorted(model_report.values()))
